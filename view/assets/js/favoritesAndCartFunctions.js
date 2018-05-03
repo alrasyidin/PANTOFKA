90b72b16a268dd794858cb939c274db94f87930e
Petra Daneva <petra.daneva95@gmail.com>
2018-05-03 16:00:10 +0300

    Cart and favorites views.

--- view/assets/js/favoritesAndCartFunctions.js
+++ view/assets/js/favoritesAndCartFunctions.js
@@ -1,3 +1,21 @@
+
+function addToFav(id ) {
+
+    var request = new XMLHttpRequest();
+    request.open("get", "handle_requests.php?target=favorites&action=addToFavorites&id=" + id );
+    request.onreadystatechange = function (ev) {
+        if(this.readyState == 4){
+            if(this.status == 200){
+                console.log(this.responseText);
+                alert(this.responseText);
+            }
+        }
+    };
+    request.send();
+
+
+}
+
 function addToCart(id ) {
 
     var sizeSelect = document.getElementById( 'sizes-for-product' + id );
@@ -22,79 +40,4 @@ function addToCart(id ) {
     };
     request.send();
 
-}
-
-function addToFav(id ) {
-
-    var request = new XMLHttpRequest();
-    request.open("get", "handle_requests.php?target=favorites&action=addToFavorites&id=" + id );
-    request.onreadystatechange = function (ev) {
-        if(this.readyState == 4){
-            if(this.status == 200){
-                console.log(this.responseText);
-                alert(this.responseText);
-            }
-        }
-    };
-    request.send();
-
-
-}
-
-function loadFavorites() {
-    var wrapper = document.getElementById("display-favorite-items");
-
-    var request = new XMLHttpRequest();
-    request.open("get", "handle_requests.php?target=favorites&action=getFavorites");
-    request.onreadystatechange = function (ev) {
-        if(this.readyState == 4){
-            if(this.status == 200){
-                wrapper.innerHTML = this.responseText;
-            }
-        }
-    };
-    request.send();
-
-}
-
-function removeItemFromFavorites(id){
-    var request = new XMLHttpRequest();
-    request.open("get", "handle_requests.php?target=favorites&action=removeFromFavorites&id=" + id );
-    request.onreadystatechange = function (ev) {
-        if(this.readyState == 4){
-            if(this.status == 200){
-                console.log(this.responseText);
-                alert(this.responseText);
-            }
-        }
-    };
-    request.send();
-}
-
-function removeItemSizeFromCart(product_id , size_no){
-    var request = new XMLHttpRequest();
-    request.open("get", "handle_requests.php?target=cart&action=removeItemSize&productId=" + product_id + '&sizeNo=' +size_no );
-    request.onreadystatechange = function (ev) {
-        if(this.readyState == 4){
-            if(this.status == 200){
-                console.log(this.responseText);
-                alert(this.responseText);
-            }
-        }
-    };
-    request.send();
-}
-
-function removeItemFromCart(product_id ){
-    var request = new XMLHttpRequest();
-    request.open("get", "handle_requests.php?target=cart&action=removeItem&productId=" + product_id);
-    request.onreadystatechange = function (ev) {
-        if(this.readyState == 4){
-            if(this.status == 200){
-                console.log(this.responseText);
-                alert(this.responseText);
-            }
-        }
-    };
-    request.send();
 }
